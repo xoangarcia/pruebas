579ab270a73309ce47081e118dcd91e4aa1413be
Xoan Garcia <xoanfuerteventura@gmail.com>
2022-03-17 18:16:11 +0100

    primer commit

---
+++ src/main/java/com/mycompany/practica10/Sumador.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.practica10;
+
+public class Sumador {
+
+    public static float sumar(float a, float b) {
+        return a + b;
+    }
+}
